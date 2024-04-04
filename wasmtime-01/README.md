8793e5db19c40525013eebbbf7db2a867b14c110
karthikeyan.jayakuma <karthikeyan.jayakumar@gamesys.ee>
2024-04-04 17:28:15 +0300

    wasmtime-01

---
+++ wasmtime-01/README.md
@@ -0,0 +1,7 @@
+A simple test, that loads wasm (lib) in Rust using wasmtime and calls a function on that lib 
+
+- wasm_add_bg.wasm : is generated using wasm-pack
+
+Reference:
+https://guptanikhil.medium.com/rust-wasm-embed-wasmtime-in-your-rust-app-51c4da4231f6
+
---
+++ wasmtime-01/src/main.rs
@@ -0,0 +1,24 @@
+use wasmtime::*;
+use wasmtime_wasi::WasiCtxBuilder;
+
+fn main() {
+    let engine = Engine::default();
+
+    let module = Module::from_file(&engine, "./wasm_add_bg.wasm").unwrap();
+
+    let mut exports = module.exports();
+    while let Some(foo) = exports.next() {
+        println!("{}", foo.name());
+    }
+
+    let linker = Linker::new(&engine);
+    let wasi = WasiCtxBuilder::new().inherit_stdio().inherit_args().build();
+    let mut store = Store::new(&engine, wasi);
+
+    let link = linker.instantiate(&mut store, &module).unwrap();
+    let add_fn = link
+        .get_typed_func::<(u32, u32), u32>(&mut store, "add_num")
+        .unwrap();
+
+    println!("{}", add_fn.call(&mut store, (1, 2)).unwrap());
+}
